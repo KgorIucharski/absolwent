f8b7422d561ac7648866e319aeaf8fc8314a978a
kgoriucharski <igor.kucharski@lo1.sandomierz.pl>
2022-09-15 10:32:47 +0200

    dodanie aplikacji osoby i dwóch widokow przykładowych

---
+++ osoby/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("JAnooooooOO0!")
+
+def about(request):
+    return HttpResponse("( ͡° ͜ʖ ͡°)💀💀💀💀💀💀💀👮🏿‍♂️ 👮🏿‍♂️ 👮🏿‍♂️ 👮🏿‍♂️ uɐǝ (▀̿̿Ĺ̯̿▀̿ ̿)ן")
